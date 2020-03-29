2366c49ec0041559a757271c1e49cc8313a5c3a9
KyleCondren <59985079+KyleCondren@users.noreply.github.com>
2020-03-29 11:01:57 -0400

    Add files via upload

--- a3_condren_problem1/src/Application.java
+++ a3_condren_problem1/src/Application.java
@@ -1,9 +1,8 @@
 
-
 public class Application {
 	public static void main(String[] args) {
 		Duplicate_Remover duplicateRemover = new Duplicate_Remover();
-		duplicateRemover.remove("C:\\Users\\Kyle\\eclipse-workspace\\a3_condren_problem1\\problem1.txt");
-		duplicateRemover.write("C:\\Users\\Kyle\\Documents\\Spring 2020\\COP 3330\\a3_condren_p1\\unique_words.txt");
+		duplicateRemover.remove("problem1.txt");
+		duplicateRemover.write("unique_words.txt");
 	}
 }
